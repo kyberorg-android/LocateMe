9f902810f914c8cda04650ed9d7b6efa7086930f
Alex Muravya <asm@virtalab.net>
2013-08-09 16:46:26 +0400

    Added Application class

---
+++ src/net/virtalab/android/locateme/MainActivity.java
@@ -0,0 +1,18 @@
+package net.virtalab.android.locateme;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * First activity user see in your app
+ */
+public class MainActivity extends FragmentActivity {
+    private static final String TAG = App.TAG+":MainActivity";
+    private static String[] TAB_LABELS;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
